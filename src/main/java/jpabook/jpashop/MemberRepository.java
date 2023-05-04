dae6090363f300e3bf29fe2d734d5d4c1c147f82
eycha <silvlexonst@gmail.com>
2023-05-04 12:39:38 +0900

    yaml edit and Memeber test code

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -16,4 +16,7 @@ public class MemberRepository {
         em.persist(member);
         return member.getId();
     }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
